05db6daca0da11ddd7e2ef2be2707bf62fc881da
Ayoub Driouich <adriouic@e1r8p8.1337.ma>
2022-05-18 23:56:42 +0100

    resolve abort

---
+++ day00/ex02/external/Account.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "external/Account.hpp"
+
+void	Account::displayAccountsInfos(void)
+{
+	
+}
+
+int main(int ac,  char	**av)
+{
+	(void)ac;
+	(void)av;
+	return (0);
+}
+
